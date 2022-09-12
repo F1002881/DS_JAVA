0f2df563cad48d75d4b46e68baaa413882ae214c
F1002881 <22axfontaine@gmail.com>
2022-09-12 12:01:10 -0500

    Set Word Tester Commit

---
+++ 1st/Set Words Assignment/WordTester.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+
+public class WordTester{
+    public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(new File("wordList.txt"));
+        int count = Integer.parseInt(scan.nextLine());
+        Word[] arr = new Word[count];
+        for(int i = 0; i < count; i++)
+        {
+            String line = scan.nextLine();
+            Word w = new Word(line);
+            arr[i] = w;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
