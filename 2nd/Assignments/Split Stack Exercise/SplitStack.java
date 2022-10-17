676c041f38d68f663f5a908a22fa2f5004363f75
F1002881 <22axfontaine@gmail.com>
2022-10-17 12:34:27 -0500

    Split Stack Commit

---
+++ 2nd/Assignments/Split Stack Exercise/SplitStack.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class SplitStack{
+    public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(new File("2nd/Assignments/Split Stack Exercise/splitStack.txt"));
+        int numStacks = Integer.parseInt(scan.nextLine());
+        
+    }
+}
